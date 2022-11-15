e8119aafcaf623d2a3b227e5d7b5d7cf2d50421e
Taha Jumaah <taharasheed95@gmail.com>
2022-11-15 21:42:57 +0100

    still working out the kinks of the logic. gonna go to bed now.

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let totalObject = {
      firstOperand: '',
      secondOperand: '',
+     operator: '',
      total: 0,
 };
 
+let firstOperand = totalObject.firstOperand;
+let secondOperand = totalObject.secondOperand;
+let operator = totalObject.operator;
+let total = totalObject.total;
 
 // Buttons Assignment
 let one = document.getElementById('1');
@@ -28,15 +33,17 @@ let equal = document.getElementById('equal');
 
 // adding Event Listeners for NUMBERS
 
-let allButtons = document.getElementsByClassName('button-17');
+let allButtons = document.querySelectorAll('.button-17');
 
 for (let index = 0; index < allButtons.length; index++) {
     const element = allButtons[index];
-    element.addEventListener('pointerdown',(  addtoScreen => 
-        calculatorWindow.value = calculatorWindow.value + element.innerHTML
-        
-        ) )
-}
+    element.addEventListener('pointerdown', addtoScreen) 
+
+        function addtoScreen() {
+            calculatorWindow.value = calculatorWindow.value + element.innerHTML
+            // console.log(this.innerHTML)
+        }}
+
 
 // Clear Window Event Listener and Function
 let calculatorWindow = document.getElementById('numbers-here')
@@ -44,49 +51,59 @@ let calculatorWindow = document.getElementById('numbers-here')
 clear.addEventListener('pointerdown', clearScreen);
 
 function clearScreen() {calculatorWindow.value = '';
-    
 }
 
 
 // EventListeners and Funcitons for Operators
 
-equal.addEventListener('pointerdown', equalFunction());
+equal.addEventListener('pointerdown', equalFunction);
 
 function equalFunction() {
-
-    if (totalObject.secondOperand !== '') {
-        calculatorWindow.value = totalObject.total;
-        
+    if (totalObject.secondOperand === '' && totalObject.firstOperand !== '') {
+        totalObject.secondOperand = Number(calculatorWindow.value);
+        clearScreen();
     }
+    if (totalObject.operator === 'plus') {
+        totalObject.total = totalObject.firstOperand + totalObject.secondOperand;
+        calculatorWindow.value = totalObject.total;
+
+    }
+    console.table(totalObject);
+
     
 }
 
 
 
 add.addEventListener('pointerdown' , function additionFunction() {
+    totalObject.operator = 'plus';
+
     if (calculatorWindow.value === '') {
         console.log('empty')
     }
-    if (totalObject.firstOperand !== '' && totalObject.secondOperand !== '') {
-        
-        totalObject.total = totalObject.firstOperand + totalObject.secondOperand;
+
+    if (firstOperand === '') {
+        total = Number(calculatorWindow.value);
+        // console.log(this.innerHTML)
+        clearScreen();
+
+    }
+
+     else if (secondOperand === '' && firstOperand !== '') {
+        secondOperand = Number(calculatorWindow.value);
         clearScreen();
     }
+
+
+
+    // if (totalObject.firstOperand !== '' && totalObject.secondOperand !== '') {
+    //     totalObject.total = totalObject.firstOperand + totalObject.secondOperand;
+    //     totalObject.firstOperand = totalObject.total;
+    //     totalObject.secondOperand = '';
+    //     calculatorWindow.value = totalObject.total;        
+    // }
+
+
     
-    if (totalObject.secondOperand !== '') {
-        totalObject.secondOperand = Number(calculatorWindow.value)
-        clearScreen();
-
-    }
-    if (totalObject.firstOperand == '') {
-        totalObject.firstOperand = Number(calculatorWindow.value);
-        clearScreen();
-
-    }
-
-    
-    console.log(totalObject.firstOperand);
-    console.log(totalObject.secondOperand);
-    console.log(totalObject.total)
-})
-
+    console.table(totalObject);
+});
